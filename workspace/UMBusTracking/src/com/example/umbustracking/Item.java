29ba66f64e334f98e476c840a88c35e660356d7d
patheon17 <quantum1711@gmail.com>
2013-10-04 18:05:52 +0800

    Revert "f"
    This reverts commit 4254f1303395c8f7ba803aa6218dae57c6840a55.

--- workspace/UMBusTracking/src/com/example/umbustracking/Item.java
+++
@@ -1,9 +0,0 @@
-package com.example.umbustracking;
-
-import android.view.LayoutInflater;
-import android.view.View;
-
-public interface Item {
-    public int getViewType();
-    public View getView(LayoutInflater inflater, View convertView);
-}
